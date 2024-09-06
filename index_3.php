d688468776aa6a24201d84998706b0eb3f18e731
Thamiris <thamirislimasp@gmail.com>
2024-09-05 22:11:39 -0300

    Atualização

--- index_3.php
+++ index_3.php
@@ -9,4 +9,17 @@ echo '<pre>';
 
 print_r($nome);
 
+$json = '{
+    "administrador" : "João Ribeiro",
+    "secretária" : "Carla Santos",
+    "diretor_geral" : "Márcio Silva",
+    "oficial_contas" : "Fernada Correia"
+}';
+
+$dados_empresa = json_decode ($json, true);
+
+echo '<pre>';
+
+print_r($dados_empresa);
+
 ?>
