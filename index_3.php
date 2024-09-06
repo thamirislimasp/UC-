ad90261c8a366028a12b01146ac4ee3d516bbb05
Thamiris <thamirislimasp@gmail.com>
2024-09-05 22:05:41 -0300

    Atualização

--- index_3.php
+++ index_3.php
@@ -1,4 +1,7 @@
 <?php
+
+// A função json_decode permite a operação contrária, ou seja, partindo de um JSON, deixando a coleção operável em PHP.
+
 $json = '["João", "Ana", "Carlos", "Martin"]';
 $nome = json_decode ($json);
 
