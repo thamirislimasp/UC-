2abdcb43b8c26a2ec0b570d0677cf6c981237a00
Thamiris <thamirislimasp@gmail.com>
2024-09-12 22:01:39 -0300

    Aula 12/09

---
+++ CLIENTE/index_2.php
@@ -0,0 +1,21 @@
+<?php
+// Chamar um Endpoint da API através do cURL (Client URL) com o protocolo HTTP e o método (verb) GET
+
+echo '<pre>';
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+CURLOPT_URL => 'http://localhost/tl/UC9/test_api_1/get_some_clients/?total_clients=5',
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => '',
+CURLOPT_MAXREDIRS => 0,
+CURLOPT_TIMEOUT => 0,
+CURLOPT_FOLLOWLOCATION => true,
+// CURLOPT_HTTP_VERSION => CURLOPT_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => 'GET',
+));
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
+
+?>
